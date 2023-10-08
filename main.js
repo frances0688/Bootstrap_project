d92611d4dbeecabde58dc3f69662f1079baab40f
Frances Morales Velilla <frances0688@gmail.com>
2023-10-08 13:57:35 +0200

    correct page redirect error of reading properties of null by addinng a conditional.  Add accordeon to home page with some investigation content.

--- main.js
+++ main.js
@@ -1,88 +1,104 @@
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password1 = document.getElementById('password1');
-const myForm = document.getElementById('myForm');
-const submitAlert = document.getElementById('submitAlert')
-const usersContainer = document.getElementById('usersContainer');
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password1 = document.getElementById("password1");
+const myForm = document.getElementById("myForm");
+const submitAlert = document.getElementById("submitAlert");
+const usersContainer = document.getElementById(
+	"usersContainer"
+);
 
-const allUsers = JSON.parse(localStorage.getItem("allUsers")) || [];
-const imgLink = "https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?w=1380&t=st=1696513773~exp=1696514373~hmac=ae54a25456b5502c33dc3667df38451d7c7498c06c88d608cb4fc95fffbac0b7"
-
+const allUsers =
+	JSON.parse(localStorage.getItem("allUsers")) || [];
+const imgLink =
+	"https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-backgroun" +
+	"d_23-2148902771.jpg?w=1380&t=st=1696513773~exp=1696514373~hmac=ae54a25456b5502c3" +
+	"3dc3667df38451d7c7498c06c88d608cb4fc95fffbac0b7";
 
 const saveUserObject = () => {
-    const user = {
-        name: name.value,
-        email: email.value,
-        password: password.value,
-        password1: password1.value,
-    };
+	const user = {
+		name: name.value,
+		email: email.value,
+		password: password.value,
+		password1: password1.value,
+	};
 
-    allUsers.push(user);
+	allUsers.push(user);
 
-    localStorage.setItem('allUsers', JSON.stringify(allUsers));
-}
+	localStorage.setItem(
+		"allUsers",
+		JSON.stringify(allUsers)
+	);
+};
 
 const createCard = () => {
-    const savedUsers = JSON.parse(localStorage.getItem("allUsers"));
-    console.log(savedUsers)
-    console.log(savedUsers[0].name)
-    savedUsers.forEach(user => {
-
-        usersContainer.innerHTML +=
-            `<div class="col-sm-4">
-                <div class="card" >
-                    <img src=${imgLink} class="card-img-top" alt="card-image-1">
-                    <div class="card-img-overlay text-center">
-                        <i class="bi bi-person-circle display-1"></i>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">${user.name}</h5>
-                        <p class="card-text">${user.email}</p>
-                    </div>
-                </div>
-            </div>`
-    });
-}
+	if (usersContainer !== null) {
+		allUsers.reverse().forEach((user) => {
+			usersContainer.innerHTML += `<div class="col-sm-4">
+            <div class="card" >
+            <img src=${imgLink} class="card-img-top" alt="card-image-1">
+            <div class="card-img-overlay text-center">
+            <i class="bi bi-person-circle display-1"></i>
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">${user.name}</h5>
+            <p class="card-text">${user.email}</p>
+            </div>
+            </div>
+            </div>`;
+		});
+	}
+};
 
 createCard();
 
 const validateData = (e) => {
-    e.preventDefault();
-    submitAlert.classList.toggle('invisible');
-    const classes = ['alert-warning', 'alert-danger', 'alert-success'];
-    const reEmail = /(\w+?@\w+?\x2E.+)/;
-    const rePassword = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/;
-    if (name.value == "" || email.value == "" || password.value == "" || password1 == "") {
-        submitAlert.classList.remove(...classes);
-        submitAlert.classList.add('alert-warning');
-        submitAlert.innerText = "Rellena todos los campos";
-    } else if (reEmail.test(email.value) !== true) {
-        submitAlert.classList.remove(...classes);
-        submitAlert.classList.add('alert-warning');
-        submitAlert.innerText = "Inserte un correo válido.";
-    } else if (rePassword.test(password.value) !== true) {
-        submitAlert.classList.remove(...classes);
-        submitAlert.classList.add('alert-warning');
-        submitAlert.innerText = "La contraseña debe incluir: un mínimo de 6 caracteres, 1 letra mayúscula, 1 letra minúscula, 1 número, y sin espacios";
-    } else if (password.value !== password1.value) {
-        submitAlert.classList.remove(...classes);
-        submitAlert.classList.add('alert-danger');
-        submitAlert.innerText = "Las contraseñas no coinciden.";
-    } else {
-        submitAlert.classList.remove(...classes);
-        submitAlert.classList.add('alert-success');
-        submitAlert.innerText = `Usuario creado correctamente.`;
-        saveUserObject()
-        createCard()
+	e.preventDefault();
+	submitAlert.classList.toggle("invisible");
+	const classes = [
+		"alert-warning",
+		"alert-danger",
+		"alert-success",
+	];
+	const reEmail = /(\w+?@\w+?\x2E.+)/;
+	const rePassword =
+		/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/;
+	if (
+		name.value == "" ||
+		email.value == "" ||
+		password.value == "" ||
+		password1 == ""
+	) {
+		submitAlert.classList.remove(...classes);
+		submitAlert.classList.add("alert-warning");
+		submitAlert.innerText = "Rellena todos los campos";
+	} else if (reEmail.test(email.value) !== true) {
+		submitAlert.classList.remove(...classes);
+		submitAlert.classList.add("alert-warning");
+		submitAlert.innerText = "Inserte un correo válido.";
+	} else if (rePassword.test(password.value) !== true) {
+		submitAlert.classList.remove(...classes);
+		submitAlert.classList.add("alert-warning");
+		submitAlert.innerText =
+			"La contraseña debe incluir: un mínimo de 6 caracteres, 1 letra mayúscula, 1 letr" +
+			"a minúscula, 1 número, y sin espacios";
+	} else if (password.value !== password1.value) {
+		submitAlert.classList.remove(...classes);
+		submitAlert.classList.add("alert-danger");
+		submitAlert.innerText = "Las contraseñas no coinciden.";
+	} else {
+		submitAlert.classList.remove(...classes);
+		submitAlert.classList.add("alert-success");
+		submitAlert.innerText = `Usuario creado correctamente.`;
+		saveUserObject();
+		createCard();
+	}
+	setTimeout(() => {
+		submitAlert.classList.toggle("invisible");
+		submitAlert.classList.contains("alert-success")
+			? window.location.replace("./usuarios.html")
+			: false;
+	}, 3000);
+};
 
-    }
-    setTimeout(() => {
-        submitAlert.classList.contains('alert-success') ? window.location.replace('./usuarios.html') : false;
-        submitAlert.classList.toggle('invisible');
-    }, 3000);
-
-}
-
-
-myForm.addEventListener('submit', validateData);
+myForm.addEventListener("submit", validateData);
